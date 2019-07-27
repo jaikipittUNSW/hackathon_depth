d8bfcdaea1c9db61cdb49547161be92a11461ed6
Nicholas Thumiger <nicholasthumiger@gmail.com>
2019-07-28 07:27:17 +1000

    The legend 27

---
+++ superSecretAlgo.py
@@ -0,0 +1,19 @@
+import User
+import Post
+import Comment
+import array as arr
+
+
+def superSecretAlgo(dict, n):
+    for x in dict:
+        maxval = 0:
+        for y in dict:
+            if x.get() > maxval:
+                maxval = x.get()
+            
+
+
+return
+
+
+
